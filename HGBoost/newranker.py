6f9ca8bce9c9d629471f3f8018cff44fa7bbad41
evageier <eva.geierstanger@gmail.com>
2025-12-05 14:01:50 -0800

    new ranking, applied to HGBoost that doesn't use a "burden" model, instead accounts for income in the second layer, weighting low income higher

---
+++ HGBoost/newranker.py
@@ -0,0 +1,91 @@
+import pandas as pd
+
+# 3 models (bill, emissions, energy) -> top 200 csvs 
+emission_top200 = pd.read_csv("HGBoost/200_test_emis.csv")
+bill_top200 = pd.read_csv("HGBoost/200_test_bill.csv")
+energy_top200 = pd.read_csv("HGBoost/200_test_elec.csv")
+orig_data = pd.read_csv("Preprocess/san_mateo_needed_cols.csv")[['bldg_id', 'in.representative_income']]
+
+# select the weights
+weights = [0.25, 0.25, 0.25, 0.25]
+# Order is Emissions, Bill, Burden, Energy
+
+# merge preds by building ID
+merged = emission_top200.merge(bill_top200, on="bldg_id", how="outer")
+merged = merged.merge(energy_top200, on="bldg_id", how="outer")
+merged = merged.merge(orig_data, on="bldg_id", how="outer") # adds in rep. income
+merged.rename(columns={'in.representative_income': 'zscored_income_rep'}, inplace=True)
+
+def z_score(val):
+    num = val - val.mean()
+    denominator = val.std()
+    if denominator == 0:
+        z_score = 0
+    else:
+        z_score = num / denominator
+    return z_score
+
+#normalize z score predictions, csvs are currently  bldg_id, score
+for col in merged.columns:
+    if col != "bldg_id":
+        merged[col] = z_score(merged[col])
+    
+#add total score column
+merged['total_score'] = (weights[0]*merged["emis_red_pred"] 
++ weights[1]*merged["bill_red_pred"]
+- weights[2]*merged["zscored_income_rep"] # higher income is punished
++ weights[3]*merged["elecsav_pred"])
+
+# sort preds descending (highest score first), reset index to show rank
+merged =merged.sort_values(by=['total_score'], ascending=False).reset_index(drop=True).head(200)
+
+# add in actual data (actual emissions, actual bill, burden, energy sav.)
+# actual data
+bill_data = pd.read_csv("Preprocess/sanmateo_bill_data.csv")
+emission_data = pd.read_csv("Preprocess/sanmateo_emis_data.csv")
+burden_data = pd.read_csv("Preprocess/sanmateo_burden_data.csv")
+energy_data = pd.read_csv("Preprocess/sanmateo_energy_data.csv")
+
+actual_data = bill_data.merge(emission_data[['bldg_id', 'out.emissions_reduction.total.aer_mid_case_avg..co2e_kg']], on="bldg_id")\
+.merge(burden_data[['bldg_id', 'out.energy_burden_savings..percentage']], on="bldg_id")\
+        .merge(energy_data[['bldg_id', 'out.electricity.net.energy_savings..kwh']], on="bldg_id")
+
+actual_data = actual_data.rename(columns={
+    "out.emissions_reduction.total.aer_mid_case_avg..co2e_kg": "actual_emiss_red",
+    'out.utility_bills.total_bill_savings..usd': 'actual_bill_sav',
+    'out.energy_burden_savings..percentage': 'actual_burd_red',
+    "out.electricity.net.energy_savings..kwh": "actual_elec_save",
+    'in.representative_income': 'actual_income_rep'})
+
+print(actual_data.head(10))
+
+merged = merged.merge(actual_data, on="bldg_id", how="left")
+
+merged["total_score"] = merged["total_score"].astype(float)
+
+merged = (
+    merged
+    .sort_values(by="total_score", ascending=False)
+    .reset_index(drop=True)
+    .head(200)
+)
+
+merged = merged[[
+    'bldg_id',
+    'emis_red_pred',
+    'actual_emiss_red',
+    'bill_red_pred',
+    'actual_bill_sav',
+    'elecsav_pred',
+    'actual_elec_save',
+    'actual_burd_red',
+    'zscored_income_rep',
+    'actual_income_rep', 
+    'total_score'
+]]
+
+#Save to output csv 
+merged.to_csv("HGBoost/newrankingwithincome.csv", index=False)
+shared = set(merged['bldg_id']).intersection(set(pd.read_csv("HGBoost/0.25_0.25_0.25_0.25_top200_HGBoost.csv")['bldg_id']))
+print(f"shared {shared} which is {len(shared)} buildings/200")
+print("Successfully ranked the top 200 from each model")
